e6d3b7e39ff0f47d26f123ff251d3fee5fd3aafe
CuongSC5 <cuongsc2411@gmail.com>
2026-04-05 15:42:20 +0700

    Revert "add frontend and all req 3."
    This reverts commit 5cc191599d0752fc39336e7abf674dba3eb48962.

--- frontend/social-media/src/hooks/index.ts
+++
@@ -1,9 +0,0 @@
-export { useAuth }          from '@/context/AuthContext'
-export { useNotifications } from '@/context/NotificationContext'
-export { usePosts }         from './usePosts'
-export {
-  useDebounce,
-  useInfiniteScroll,
-  useLocalStorage,
-  useClickOutside,
-} from './useUtils'
